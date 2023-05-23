7f7d5a19b4859292b1d04021b430dc5e2698f821
konapun <bremen.braun@gmail.com>
2023-05-22 23:49:23 -0500

    Change output type

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   images: {
-    loader: 'akamai',
-    path: '',
+    loader: "akamai",
+    path: "",
   },
-  assetPrefix: isProd ? '/website' : './',
-}
+  assetPrefix: isProd ? "/website" : "./",
+  output: "export",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
