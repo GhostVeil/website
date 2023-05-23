22f0bf17b0816198c719ba44d0249ee8c77cee3e
konapun <bremen.braun@gmail.com>
2023-05-23 17:21:51 -0500

    unset assetPrefix for domain change

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   images: {
     unoptimized: true,
     loader: "akamai",
     path: "",
   },
-  assetPrefix: isProd ? "/website" : undefined,
   output: "export",
 };
 
