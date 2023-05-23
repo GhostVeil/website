a4e27d9b8a4298c08b91dc32e87ef6e97f0dc431
konapun <bremen.braun@gmail.com>
2023-05-22 23:06:49 -0500

    Changes for github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+  assetPrefix: isProd ? '/website/' : '',
+  images: {
+    unoptimized: true,
+  },
+}
 
 module.exports = nextConfig
