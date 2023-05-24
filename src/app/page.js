8cbc10eb71068be81b18612cea61741e91afa205
konapun <bremen.braun@gmail.com>
2023-05-23 20:59:48 -0500

    Add glow

--- src/app/page.js
+++ src/app/page.js
@@ -11,17 +11,17 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-ghost-black">
       <div className="relative flex place-items-center flex-col">
         <Image
-          className="relative"
-          src="./GhostVeil.png"
+          className="relative rounded-full bg-ghost-black--glow shadow-[0_0_60px_30px_#2a2a2a,0_0_100px_60px_#1a1a1a,0_0_140px_90px_#161616]"
+          src="./logo.png"
           alt="GhostVeil Logo"
           width={400}
           height={400}
           priority
         />
-        <div className={`text-6xl ${font.className}`}>
+        <div className={`text-6xl z-10 ${font.className}`}>
           GhostVeil
         </div>
-        <p className="text-white text-opacity-60">
+        <p className="text-white text-opacity-60 z-10">
           Coming soon
         </p>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        'ghost-black': '#161616'
+        'ghost-black': '#161616',
+        'ghost-black--glow': "#2a2a2a",
       }
     },
   },
